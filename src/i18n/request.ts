d2f381c04035242a2449ed805d5627cd3a2c00f3
Amin Zamani Afshar <zamani35099@gmail.com>
2026-02-24 16:35:22 +0330

    hot fix 404 error

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,8 +1,8 @@
 import { getRequestConfig } from 'next-intl/server';
 import { routing } from './routing';
 
-export default getRequestConfig(async ({ locale }) => {
-  const requested = locale ?? routing.defaultLocale;
+export default getRequestConfig(async ({ requestLocale }) => {
+  const requested = (await requestLocale) ?? routing.defaultLocale;
   const activeLocale = routing.locales.includes(requested as 'fa' | 'en') ? requested : routing.defaultLocale;
 
   return {
