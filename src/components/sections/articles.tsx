9a38c014f5d62dbd75b92e0c66f5c97067ad0c48
Amin Zamani Afshar <zamani35099@gmail.com>
2026-02-28 00:43:45 +0330

    delete previous design

--- src/components/sections/articles.tsx
+++
@@ -1,9 +0,0 @@
-import { externalArticles } from '@/data/site';
-import { getArticles } from '@/lib/articles';
-import { ArticlesClientSection } from '@/components/sections/articles-client';
-
-export async function ArticlesSection({ locale }: { locale: 'fa' | 'en' }) {
-  const onsite = await getArticles(locale);
-
-  return <ArticlesClientSection locale={locale} onsite={onsite} external={externalArticles} />;
-}
